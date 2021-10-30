9c54db5d4de2eaf961a0c3a237aec471a617c7b8
David Han <davidhan.5.25@gmail.com>
2021-10-29 21:34:31 -0700

    serve static assets in production

--- server/index.js
+++ server/index.js
@@ -15,6 +15,12 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/characters", require("./routes/characters"));
 
-app.get("/", (req, res) => res.send("API is running."));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
